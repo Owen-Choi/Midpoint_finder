3c03d1e156f63a55d4f61036f6231383b674676b
Owen-Choi <oldstyle482@gmail.com>
2021-11-16 02:29:30 +0900

    데이터베이스 수정 및 역 정보 클래스 초기화 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static String url = "jdbc:mysql://localhost:3306/Alg_term?useUnicode=true&characterEncoding=utf8";
+    static String url = "jdbc:mysql://localhost:3306/algorithm_termproject?useUnicode=true&characterEncoding=utf8";
     static String userName = "root";
     static String password = "12345";
     static int userNum;
     static user[] users;
+    static Station_info[] stations;
     public static void main(String[] args) throws Exception {
         // mysql 서버와 연결. 이 connection을 통해 쿼리 보내고 결과 받음
         //Class.forName("com.mysql.jdbc.Driver");
@@ -20,60 +21,87 @@ public class Main {
         System.out.println("Please Enter the number of user : ");
         userNum = Integer.parseInt(br.readLine());
         users = new user[userNum];
+        // select * from Station_info 의 정보 담아주기
+        //Init_setter(resultSet);
+        PreparedStatement TogetSize = connection.prepareStatement("SELECT count(*) AS size FROM station_info");
+        ResultSet Size =  TogetSize.executeQuery();
+        int size = Size.getInt(1);
+        // 이제 역의 전체 수를 얻어왔으니 전체 역 정보를 담을 배열을 초기화 하겠습니다.
+        stations = new Station_info[size];
+        // 이제 전체 역의 정보를 위 배열에 차례차례 넣겠습니다.
+        PreparedStatement statement = connection.prepareStatement("SELECT * AS size FROM station_info");
+        ResultSet resultSet = statement.executeQuery();
+        Init_setter(resultSet);
+
         for(int i=0; i<userNum; i++) {
             System.out.println("Please Enter the location of user " + i);
             String tempName = br.readLine();
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM station_info WHERE Name = ?");
-            statement.setString(1, tempName);
-            ResultSet resultSet = statement.executeQuery();
-            while(resultSet.next()) {
-                String tempLine =resultSet.getString(1);
-                String tn = resultSet.getString(2);
-                float lat = resultSet.getFloat(3);
-                float lon = resultSet.getFloat(4);
-                boolean fin = resultSet.getBoolean(5);
-                System.out.println(tempLine + " " + tn + " " + lat + " " + lon + " " + fin);
-            }
+            //PreparedStatement statement = connection.prepareStatement("SELECT * FROM station_info WHERE Station_name = ?");
+            //PreparedStatement statement = connection.prepareStatement("SELECT * AS size FROM station_info");
+            //statement.setString(1, tempName);
+            //ResultSet resultSet = statement.executeQuery();
+
         }
     }
+
+    static void Init_setter(ResultSet resultSet) throws SQLException {
+        String tempLine, name;
+        float lat, lon;
+        int cafe, store, sum_around;
+        boolean dptstore, cinema, is_final;
+        int iter = 0;
+        while(resultSet.next()) {
+            tempLine = resultSet.getString(1);
+            name = resultSet.getString(2);
+            lat = resultSet.getFloat(3);
+            lon = resultSet.getFloat(4);
+            cafe = resultSet.getInt(5);
+            store = resultSet.getInt(6);
+            sum_around = resultSet.getInt(7);
+            dptstore = resultSet.getBoolean(8);
+            cinema = resultSet.getBoolean(9);
+            is_final = resultSet.getBoolean(10);
+            stations[iter] = new Station_info(tempLine, name, lat, lon, cafe,
+                    store, sum_around, dptstore, cinema, is_final);
+            iter++;
+        }
+    }
+
     // 역간 이동에 대한 클래스. 호선정보, 출발역, 도착역, 거리, 종착역 여부를 정보로 갖는다.
-    static class Station_Move {
-        String Line;
-        String Station_name;
-        String Next_station;
-        float distance;
-        public Station_Move(String Line, String Station_name, String Next_station, float distance) {
-            this.Line = Line;
-            this.Station_name = Station_name;
-            this.Next_station = Next_station;
-            this.distance = distance;
-        }
-    }
-    // user 클래스. 처음 출발하는 역 노드, 누적 거리값, 여태 지나온 역들을 담을 queue를 가진다.
-    static class user {
-        Station_info Current_station;
-        Queue<Station_info> Node_queue;
-        float Cumulated_dist;
-        public user(Station_info Current_station, float Cumulated_dist) {
-            this.Current_station = Current_station;
-            this.Cumulated_dist = Cumulated_dist;
-            Node_queue = new LinkedList<>();
-        }
-    }
     // 역 정보 클래스. 역의 이름과 좌표값을 가진다.
     static class Station_info {
         String Line_Info;
         String Station_name;
-        float x, y;
-        boolean Is_Final;
-        public Station_info(String Line_Info, String Station_name, float x, float y, boolean Is_Final) {
+        float lat, lon;
+        int cafe, store, sum_around;
+        boolean dptstore, cinema, Is_Final;
+        public Station_info(String Line_Info, String Station_name, float x, float y, int cafe, int store,
+                           int sum_around, boolean dptstore, boolean cinema, boolean Is_Final) {
             this.Line_Info = Line_Info;
             this.Station_name = Station_name;
-            this.x = x;
-            this.y = y;
+            this.lat = x;
+            this.lon = y;
+            this.cafe = cafe;
+            this.store = store;
+            this.sum_around = sum_around;
+            this.dptstore = dptstore;
+            this.cinema = cinema;
             this.Is_Final = Is_Final;
         }
     }
-
+    //환승체크
+    // 출발지 검색
+    //
+}
+// user 클래스. 처음 출발하는 역 노드, 누적 거리값, 여태 지나온 역들을 담을 queue를 가진다.
+class user {
+    Main.Station_info Current_station;
+    Queue<Main.Station_info> Node_queue;
+    float Cumulated_dist;
+    public user(Main.Station_info Current_station, float Cumulated_dist) {
+        this.Current_station = Current_station;
+        this.Cumulated_dist = Cumulated_dist;
+        Node_queue = new LinkedList<>();
+    }
 }
 
